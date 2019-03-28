0ebbf81047089376a0c8593dff418f83e795f77c
oriefy <shahzaibakash@gmail.com>
2019-03-28 16:12:25 +0500

    add patient model and admin

--- hms/admin.py
+++ hms/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from hms.models import Patient
 
 # Register your models here.
+class PatientAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+    list_display = ('name', 'age', 'gender', )
+    list_filter = ('gender',)
--- hms/models.py
+++ hms/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Patient(models.Model):
+    name = models.CharField(max_length=20)
+    age = models.IntegerField()
+    address = models.CharField(max_length=50)
+
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+
+    def __str__(self):
+        return self.name
