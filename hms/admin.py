0de6a25858eaa37b271ef89b91d22f51b434d376
oriefy <shahzaibakash@gmail.com>
2019-03-28 16:32:13 +0500

    add admin classes for reciept and patient

--- hms/admin.py
+++ hms/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
-from hms.models import Patient
+from hms.models import Patient, Reciept
+
 
 # Register your models here.
+@admin.register(Patient)
 class PatientAdmin(admin.ModelAdmin):
     search_fields = ('name',)
     list_display = ('name', 'age', 'gender', )
     list_filter = ('gender',)
+
+
+@admin.register(Reciept)
+class RecieptAdmin(admin.ModelAdmin):
+    raw_id_fields = ('patient',)
+    search_fields = ('patient__name',)
