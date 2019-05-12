8881dfc9ba310630f047f9edd2b45cce7b81cbdd
oriefy <shahzaibakash@gmail.com>
2019-05-12 15:28:57 +0500

    display image

--- django_hospital/urls.py
+++ django_hospital/urls.py
@@ -15,7 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+
+admin.site.site_header = "Niaz Medicare Admin"
+admin.site.site_title = "Niaz Medicare Portal"
+admin.site.index_title = "Welcome to Niaz Medicare Portal"
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- hms/admin.py
+++ hms/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from hms.models import Patient, Reciept
+from django.utils.html import mark_safe
 
 
 # Register your models here.
@@ -12,5 +13,17 @@ class PatientAdmin(admin.ModelAdmin):
 
 @admin.register(Reciept)
 class RecieptAdmin(admin.ModelAdmin):
+    readonly_fields = ["headshot_image",]
+
+    def headshot_image(self, obj):
+        return mark_safe('<img src="{url}" width="{width}" height={height} />'.format(
+            url = obj.reciept.url,
+            width=obj.reciept.width,
+            height=obj.reciept.height,
+            )
+    )
+    empty_value_display = '--Empty--'
+
+
     raw_id_fields = ('patient',)
     search_fields = ('patient__name',)
